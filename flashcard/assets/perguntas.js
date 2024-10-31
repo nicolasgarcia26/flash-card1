ccd238ffc5afeae30f06f742a9ae5f0699b8af0e
nicolasgarcia26 <130596951+nicolasgarcia26@users.noreply.github.com>
2024-10-30 21:20:04 -0300

    Add files via upload

---
+++ flashcard/assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Futebol',
+    'Que time ganhou a Libertadores de 2011?',
+    'O time campeão foi o brasileiro Santos'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
